1e463b0d7d85a80ba5f0e6032d0c39b45909c675
Evgenii Nikitin <evgeniinikitin85@gmail.com>
2022-11-08 00:31:38 +0400

    первая и вторая задачи первого урока

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  let i = 1;
+
+  do {
+    result = result * i;
+    i++;
+  }
+  while (i <= n);
+
+  return result;
 }
+
+let meaning = prompt ('введите значение');
+
+alert(`результат ${factorial(+meaning)}`);
