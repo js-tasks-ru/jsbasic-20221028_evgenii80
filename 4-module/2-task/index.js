5972605d072a4cc366d3c250f8ab828b04840994
Evgenii Nikitin <evgeniinikitin85@gmail.com>
2022-11-25 14:41:38 +0100

    4 modul all tasks

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,6 +1,6 @@
 function makeDiagonalRed(table) {
  	for (let i = 0; i < table.rows.length; i++) {
 	  let row = table.rows[i];
-	  row.cells[i].style.backgroundColor = 'red'
+	  row.cells[i].style.backgroundColor = 'red';
   }
 }
